449a0bf36d77a000d80bd65644456867d77ae8bb
ame <hey@amegroups.com>
2022-01-24 14:36:04 +0800

    # add qrocde

--- app.go
+++ app.go
@@ -10,8 +10,10 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 type file struct {
@@ -19,32 +21,32 @@ type file struct {
 	File os.File
 }
 
-// 获取本机网卡IP
-func getLocalIP() (ipv4 string, err error) {
-	var (
-		addrs   []net.Addr
-		addr    net.Addr
-		ipNet   *net.IPNet // IP地址
-		isIpNet bool
-	)
-	// 获取所有网卡
-	if addrs, err = net.InterfaceAddrs(); err != nil {
-		return
+func getLocalIpV4() string {
+	inters, err := net.Interfaces()
+	if err != nil {
+		panic(err)
 	}
-	// 取第一个非lo的网卡IP
-	for _, addr = range addrs {
-		// 这个网络地址是IP地址: ipv4, ipv6
-		if ipNet, isIpNet = addr.(*net.IPNet); isIpNet && !ipNet.IP.IsLoopback() {
-			// 跳过IPV6
-			if ipNet.IP.To4() != nil {
-				ipv4 = ipNet.IP.String() // 192.168.1.1
-				return
+	for _, inter := range inters {
+		// 判断网卡是否开启，过滤本地环回接口
+		if inter.Flags&net.FlagUp != 0 && !strings.HasPrefix(inter.Name, "lo") {
+			// 获取网卡下所有的地址
+			addrs, err := inter.Addrs()
+			if err != nil {
+				continue
+			}
+			for _, addr := range addrs {
+				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					//判断是否存在IPV4 IP 如果没有过滤
+					if ipnet.IP.To4() != nil {
+						return ipnet.IP.String()
+					}
+				}
 			}
 		}
 	}
-
-	return
+	return ""
 }
+
 func openBrowser(url string) {
 	var err error
 
@@ -67,7 +69,7 @@ func openBrowser(url string) {
 	}
 }
 
-func upload(writer http.ResponseWriter, r *http.Request) {
+func apiUpload(writer http.ResponseWriter, r *http.Request) {
 
 	r.ParseMultipartForm(200)
 	mForm := r.MultipartForm
@@ -118,9 +120,73 @@ func download(writer http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(writer, h)
 }
 
+func Upload(writer http.ResponseWriter, r *http.Request) {
+	//path, err := os.Executable()
+	exPath, err := getCurrentAbPath()
+	if err != nil {
+		log.Println(err)
+	}
+	//exPath := filepath.Dir(_path)
+	fmt.Println("current path:", exPath)
+
+	tmpl, err := template.ParseFiles(exPath + "/web/upload.tmpl")
+	if err != nil {
+		fmt.Println("create template failed, err:", err)
+		return
+	}
+	h := r.Host
+	tmpl.Execute(writer, h)
+}
+
+// 最终方案-全兼容
+func getCurrentAbPath() (string, error) {
+	dir := getCurrentAbPathByExecutable()
+	tmpDir, _ := filepath.EvalSymlinks(os.TempDir())
+	if strings.Contains(dir, tmpDir) {
+		return getCurrentAbPathByCaller(), nil
+	}
+	return dir, nil
+}
+
+// 获取当前执行文件绝对路径
+func getCurrentAbPathByExecutable() string {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, _ := filepath.EvalSymlinks(filepath.Dir(exePath))
+	return res
+}
+
+// 获取当前执行文件绝对路径（go run）
+func getCurrentAbPathByCaller() string {
+	var abPath string
+	_, filename, _, ok := runtime.Caller(0)
+	if ok {
+		abPath = path.Dir(filename)
+	}
+	return abPath
+}
+
+func SendJqueryJs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.RequestURI)
+	exPath, err := getCurrentAbPath()
+	if err != nil {
+		log.Println(err)
+	}
+	data, err := ioutil.ReadFile(exPath + "/web/qrcode.js")
+	if err != nil {
+		http.Error(w, "Couldn't read file", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+	w.Write(data)
+}
+
 func main() {
 
-	dir, er := filepath.Abs(filepath.Dir(os.Args[0]))
+	//dir, er := filepath.Abs(filepath.Dir(os.Args[0]))
+	dir, er := getCurrentAbPath()
 	if er != nil {
 		log.Fatal(er)
 	}
@@ -128,17 +194,17 @@ func main() {
 		err = os.Mkdir(dir+"/public/", 0777)
 		// TODO: handle error
 	}
-
-	ip, _ := getLocalIP()
+	ip := getLocalIpV4()
 	openBrowser("http://" + ip + ":9876")
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 
-		path, err := os.Executable()
+		//path, err := os.Executable()
+		exPath, err := getCurrentAbPath()
 		if err != nil {
 			log.Println(err)
 		}
-		exPath := filepath.Dir(path)
+		//exPath := filepath.Dir(_path)
 		fmt.Println("current path:", exPath)
 
 		tmpl, err := template.ParseFiles(exPath + "/web/index.tmpl")
@@ -147,11 +213,13 @@ func main() {
 			return
 		}
 		h := request.Host
-		tmpl.Execute(writer, h)
+		tmpl.Execute(writer, "http://"+h+"/upload")
 	})
 
-	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/api/upload", apiUpload)
+	http.HandleFunc("/upload", Upload)
 	http.HandleFunc("/download", download)
+	http.HandleFunc("/qrcode.js", SendJqueryJs)
 
 	err := http.ListenAndServe(":9876", nil)
 	if err != nil {
